5c9ccc19b60622251636097859620cf5332147d6
elghonerox <hamzaelghomri@gmail.com>
2025-11-28 20:34:34 +0100

    Fix: Simplify backend startup script

--- start-backend.bat
+++ start-backend.bat
@@ -3,14 +3,12 @@ echo Starting TruthGraph Backend API Server...
 echo.
 
 echo Installing dependencies...
-cd ..
-pip install -q -r requirements.txt
-cd backend
-pip install -q -r requirements.txt
+pip install -q aiohttp pydantic requests fastapi uvicorn websockets python-multipart substrate-interface cryptography
 
 echo.
 echo Starting server on http://localhost:8000
 echo Press Ctrl+C to stop
 echo.
 
+cd backend
 python main.py
